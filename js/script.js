31707a9c2563919a28e4f238eb62cc55f991f2b8
Driton-Gashi <69894595+Driton-Gashi@users.noreply.github.com>
2021-06-22 19:22:33 +0200

    git commit

--- js/script.js
+++ js/script.js
@@ -22,7 +22,40 @@ var navLinks = document.querySelectorAll('header ul a li');
 
 // for (let i = 0; i < navLinks.length; i++) {
 //     navLinks[i].addEventListener("click", function(){
-//     navBar.style.top='-100%';
+//     navBar.style.top='-100vh';
 
 //     } )
 // }
+
+var portofolioTitle = document.querySelectorAll('.portofolio div span h2');
+var image = document.querySelectorAll('.image');
+
+for (let i = 0; i < image.length; i++) {
+    image[i].addEventListener("mouseover",titleshow);
+    image[i].addEventListener("mouseout",titlehide);
+
+    function titleshow() {
+        portofolioTitle[i].style.opacity='1';
+    }
+    function titlehide() {
+        portofolioTitle[i].style.opacity='0';
+        
+    }
+}
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
